05995dc1c3d0324349b5fd6f374d151d1b0ef753
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-07-18 01:55:54 +0200

    Added sendBatteryInfo()

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -2,9 +2,25 @@
 #include <Preferences.h>
 #include <NMEA2000_CAN.h>
 #include <N2kMessages.h>
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
 
 #define ESP32_CAN_TX_PIN GPIO_NUM_5  // Set CAN TX port to 5 
 #define ESP32_CAN_RX_PIN GPIO_NUM_4  // Set CAN RX port to 4
+
+#define ADCpin1 34                   // GPIO 34 (Analog input, ADC1_CH6)
+#define ADC_Calibration_Value1 250.0 // For resistor measure 5 Volt and 180 Ohm equals 100% plus 1K resistor.
+
+#define ADCpin2 35                   // GPIO 35 (Analog input, ADC1_CH7))
+#define ADC_Calibration_Value2 34.3  // To measure battery voltage. The real value depends on the true resistor values for the ADC input (100K / 27 K).
+
+
+// Set time offsets
+#define SlowDataUpdatePeriod 1000  // Time between CAN Messages sent
+#define BatteryVoltageSendOffset 0
+#define WaterTankLevelSendOffset 50
+
+Adafruit_BMP280 bmp;
 
 int NodeAddress;            // To store last Node Address
 Preferences preferences; 
@@ -17,6 +33,10 @@ onst unsigned long TransmitMessages[] PROGMEM = {
 
 // Define all Function Codes
 void CheckSourceAddressChange();
+void sendBatteryInfo(void);
+bool IsTimeToUpdate(unsigned long NextUpdate);
+unsigned long InitNextUpdate(unsigned long Period, unsigned long Offset = 0);
+void SetNextUpdate(unsigned long & NextUpdate, unsigned long Period);
 
 void setup() {
   
@@ -27,6 +47,25 @@ void setup() {
   // Init USB serial port
   Serial.begin(115200);
   delay(10);
+
+  if (!bmp.begin();) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+
+    bmp.setSampling(
+      Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+      Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+      Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+      Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+      Adafruit_BMP280::STANDBY_MS_500   /* Standby time. */
+    );
+  }
 
   // Reserve enough buffer for sending all messages.
   NMEA2000.SetN2kCANMsgBufSize(8);
@@ -69,6 +108,17 @@ void setup() {
 }
 
 void loop() {
+
+  sendBatteryInfo();
+  
+  NMEA2000.ParseMessages();
+
+  CheckSourceAddressChange();
+  
+  // Dummy to empty input buffer to avoid board to stuck with e.g. NMEA Reader
+  if ( Serial.available() ) {
+    Serial.read();
+  }
 }
 
 void CheckSourceAddressChange() {
@@ -81,4 +131,53 @@ void CheckSourceAddressChange() {
     preferences.end();
     Serial.printf("Address Change: New Address=%d\n", SourceAddress);
   }
+}
+
+void sendBatteryInfo(void) {
+  static unsigned long SlowDataUpdated = InitNextUpdate(SlowDataUpdatePeriod, BatteryVoltageSendOffset);
+  tN2kMsg N2kMsg;
+  double BatteryVoltage;
+  double BatteryCurrent;
+  float BatteryTemperature;
+
+  if ( IsTimeToUpdate(SlowDataUpdated) ) {
+    SetNextUpdate(SlowDataUpdated, SlowDataUpdatePeriod);
+
+    // Read BatteryVoltage on ADCpin1
+    BatteryVoltage = ReadADC(ADCpin1) * ADC_Calibration_Value1 / 4096;
+
+    // Read BatteryCurrent on ADCpin2
+    BatteryCurrent = ReadADC(ADCpin2) * ADC_Calibration_Value2 / 4096;
+
+    // Read BatteryTemperature on BMP280
+    BatteryTemperature = bmp.readTemperature();
+
+    // Print values
+    Serial.printf(
+      "Battery: Voltage: %3.1f V, Current %3.1f A, Temperature: %3.1f \n",
+      BatteryVoltage, BatteryCurrent, BatteryTemperature
+    );
+
+    // Set N2K message
+    SetN2kPGN127508(N2kMsg, 0, BatteryVoltage, BatteryCurrent, BatteryTemperature, 0);
+
+    // Send message
+    NMEA2000.SendMsg(N2kMsg);
+  }
+}
+
+bool IsTimeToUpdate(unsigned long NextUpdate) {
+  return (NextUpdate < millis());
+}
+
+
+//*****************************************************************************
+unsigned long InitNextUpdate(unsigned long Period, unsigned long Offset = 0) {
+  return millis() + Period + Offset;
+}
+
+
+//*****************************************************************************
+void SetNextUpdate(unsigned long & NextUpdate, unsigned long Period) {
+  while ( NextUpdate < millis() ) NextUpdate += Period;
 }
