70348fada1a610afb54e5826a4d51192c3b53dae
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-07-18 01:42:12 +0200

    Added N2k Info

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -1,18 +1,69 @@
 #include <Arduino.h>
+#include <Preferences.h>
+#include <NMEA2000_CAN.h>
+#include <N2kMessages.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define ESP32_CAN_TX_PIN GPIO_NUM_5  // Set CAN TX port to 5 
+#define ESP32_CAN_RX_PIN GPIO_NUM_4  // Set CAN RX port to 4
+
+int NodeAddress;            // To store last Node Address
+Preferences preferences; 
+
+onst unsigned long TransmitMessages[] PROGMEM = {
+  127508L, // Battery Status
+  127505L, // Fluid level
+  0
+};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  uint8_t chipid[6];
+  uint32_t id = 0;
+  int i = 0;
+
+  // Init USB serial port
+  Serial.begin(115200);
+  delay(10);
+
+  // Reserve enough buffer for sending all messages.
+  NMEA2000.SetN2kCANMsgBufSize(8);
+  NMEA2000.SetN2kCANReceiveFrameBufSize(150);
+  NMEA2000.SetN2kCANSendFrameBufSize(150);
+
+  // Generate unique number from chip id
+  esp_efuse_mac_get_default(chipid);
+  for (i = 0; i < 6; i++) id += (chipid[i] << (7 * i));
+
+  // Set product information
+  NMEA2000.SetProductInformation(
+    "1", // Manufacturer's Model serial code
+    100, // Manufacturer's product code
+    "My Sensor Module",  // Manufacturer's Model ID
+    "1.0.2.25 (2019-07-07)",  // Manufacturer's Software version code
+    "1.0.2.0 (2019-07-07)" // Manufacturer's Model version
+  );
+
+  // Set device information
+  NMEA2000.SetDeviceInformation(
+    id, // Unique number. Use e.g. Serial number.
+    132, // Device function=Analog to NMEA 2000 Gateway. See codes on http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
+    25, // Device class=Inter/Intranetwork Device. See codes on  http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
+    2046 // Just choosen free from code list on http://www.nmea.org/Assets/20121020%20nmea%202000%20registration%20list.pdf
+  );
+
+  preferences.begin("nvs", false);                          // Open nonvolatile storage (nvs)
+  NodeAddress = preferences.getInt("LastNodeAddress", 34);  // Read stored last NodeAddress, default 34
+  preferences.end();
+  Serial.printf("NodeAddress=%d\n", NodeAddress);
+
+  // If you also want to see all traffic on the bus use N2km_ListenAndNode instead of N2km_NodeOnly below
+  NMEA2000.SetMode(tNMEA2000::N2km_NodeOnly, NodeAddress);
+  NMEA2000.ExtendTransmitMessages(TransmitMessages);
+
+  NMEA2000.Open();
+
+  delay(200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
