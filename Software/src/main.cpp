c5ad0a70555c5f6357ecc30309ba92dd8f225c4f
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-07-18 02:05:06 +0200

    Cleaned Code and added readADC()

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -25,7 +25,7 @@ Adafruit_BMP280 bmp;
 int NodeAddress;            // To store last Node Address
 Preferences preferences; 
 
-onst unsigned long TransmitMessages[] PROGMEM = {
+const unsigned long TransmitMessages[] PROGMEM = {
   127508L, // Battery Status
   127505L, // Fluid level
   0
@@ -37,6 +37,7 @@ void sendBatteryInfo(void);
 bool IsTimeToUpdate(unsigned long NextUpdate);
 unsigned long InitNextUpdate(unsigned long Period, unsigned long Offset = 0);
 void SetNextUpdate(unsigned long & NextUpdate, unsigned long Period);
+double ReadADC(byte pin);
 
 void setup() {
   
@@ -48,7 +49,7 @@ void setup() {
   Serial.begin(115200);
   delay(10);
 
-  if (!bmp.begin();) {
+  if (!bmp.begin()) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
                       "try a different address!"));
     Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
@@ -170,14 +171,18 @@ bool IsTimeToUpdate(unsigned long NextUpdate) {
   return (NextUpdate < millis());
 }
 
-
-//*****************************************************************************
-unsigned long InitNextUpdate(unsigned long Period, unsigned long Offset = 0) {
+unsigned long InitNextUpdate(unsigned long Period, unsigned long Offset) {
   return millis() + Period + Offset;
 }
 
-
-//*****************************************************************************
 void SetNextUpdate(unsigned long & NextUpdate, unsigned long Period) {
   while ( NextUpdate < millis() ) NextUpdate += Period;
 }
+
+double ReadADC(byte pin) {
+  double reading = analogRead(pin); // Reference voltage is 3v3 so maximum reading is 3v3 = 4095 in range 0 to 4095
+  if (reading < 1 || reading > 4095) return 0;
+  // return -0.000000000009824 * pow(reading,3) + 0.000000016557283 * pow(reading,2) + 0.000854596860691 * reading + 0.065440348345433;
+  return (-0.000000000000016 * pow(reading, 4) + 0.000000000118171 * pow(reading, 3) - 0.000000301211691 * pow(reading, 2) + 0.001109019271794 * reading + 0.034143524634089) * 1000;
+} // Added an improved polynomial, use either, comment out as required
+
